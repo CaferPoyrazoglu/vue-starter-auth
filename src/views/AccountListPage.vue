d85d17ac12eee3e5c5aa191edd2eb4a919569ce1
Cafer <cafererhan@gmail.com>
2023-10-24 00:45:35 +0300

    fix

--- src/views/AccountListPage.vue
+++ src/views/AccountListPage.vue
@@ -4,6 +4,12 @@ import axiosInstance from '@/api/axiosInstance'
 import { Modal } from 'flowbite-vue'
 import { useMessageStore } from '@/stores'
 
+const messageStore = useMessageStore()
+const isUpdateModal = ref(false)
+const isCreateModal = ref(false)
+let accountList = ref(null)
+let companyList = ref(null)
+
 const selectedAccount = ref({
     id: '',
     title: '',
@@ -13,16 +19,19 @@ const selectedAccount = ref({
     details: '',
 })
 
-const messageStore = useMessageStore()
-const isUpdateModal = ref(false)
-let accountList = ref(null)
-let companyList = ref(null)
+const newAccount = ref({
+    targetCompanyId: '',
+    accountTitle: '',
+    accountName: '',
+    accountDetails: '',
+    accountPassword: '',
+})
 
 await fetchAccounts()
 await fetchCompanies()
 
-function closeUpdateModal() {
-    isUpdateModal.value = false
+function showCreateNewAccountModal() {
+    isCreateModal.value = true
 }
 
 function showUpdateModal(account) {
@@ -33,7 +42,64 @@ function showUpdateModal(account) {
     selectedAccount.value.company = account.company
     selectedAccount.value.details = account.details
 }
+function closeUpdateModal() {
+    isUpdateModal.value = false
+}
 
+function closeCreateModal() {
+    isCreateModal.value = false
+}
+
+async function fetchAccounts() {
+  try {
+    const response = await axiosInstance.get('account/manage', {
+      withCredentials: true,
+    })
+
+    accountList.value = response.data
+  } catch (error) {
+    messageStore.status = true
+    if (error.response) {
+      messageStore.message = error.response.data.toString()
+    } else if (error.request) {
+      messageStore.message = 'Sunucuya bağlanılamıyor. Lütfen daha sonra tekrar deneyin.'
+    } else {
+      messageStore.message = 'İsteğiniz gerçekleştirilirken bir hata ile karşılaşıldı.'
+    }
+  }
+}
+
+async function createAccount() {
+    try {
+        await axiosInstance.post(
+            'account/new',
+            {
+                targetCompanyId: newAccount.value.targetCompanyId,
+                accountTitle: newAccount.value.accountTitle,
+                accountName: newAccount.value.accountName,
+                accountDetails: newAccount.value.accountDetails,
+                accountPassword: newAccount.value.accountPassword,
+                userIdList: [],
+            },
+            {
+                withCredentials: true,
+            }
+        )
+
+        await fetchAccounts()
+        closeCreateModal()
+    } catch (error) {
+        messageStore.status = true
+        if (error.response) {
+            messageStore.message = error.response.data.toString()
+        } else if (error.request) {
+            messageStore.message = 'Sunucuya bağlanılamıyor. Lütfen daha sonra tekrar deneyin.'
+        } else {
+            messageStore.message = 'İsteğiniz gerçekleştirilirken bir hata ile karşılaşıldı.'
+        }
+        closeCreateModal()
+    }
+}
 async function updateAccount() {
     try {
         await axiosInstance.put(
@@ -64,24 +130,7 @@ async function updateAccount() {
     }
 }
 
-async function fetchAccounts() {
-    try {
-        const response = await axiosInstance.get('account/all', {
-            withCredentials: true,
-        })
 
-        accountList.value = response.data
-    } catch (error) {
-        messageStore.status = true
-        if (error.response) {
-            messageStore.message = error.response.data.toString()
-        } else if (error.request) {
-            messageStore.message = 'Sunucuya bağlanılamıyor. Lütfen daha sonra tekrar deneyin.'
-        } else {
-            messageStore.message = 'İsteğiniz gerçekleştirilirken bir hata ile karşılaşıldı.'
-        }
-    }
-}
 
 async function fetchCompanies() {
     try {
@@ -130,7 +179,14 @@ async function deleteAccount() {
             <h2 class="text-4xl font-extrabold">Hesap Ayarları</h2>
             <hr class="h-px bg-transparent border-0" />
 
-            <div class="columns-2 py-5"></div>
+            <div class="columns-2 py-5">
+                <button
+                    class="full text-white bg-green-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    type="submit"
+                    @click="showCreateNewAccountModal()">
+                    Yeni Hesap Ekle
+                </button>
+            </div>
 
             <div class="relative overflow-x-auto sm:rounded-lg">
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -209,7 +265,7 @@ async function deleteAccount() {
                 <div class="mb-6">
                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Bilgi</label>
                     <input
-                        id="username"
+                        id="details"
                         v-model="selectedAccount.details"
                         class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                         placeholder=""
@@ -251,6 +307,87 @@ async function deleteAccount() {
                             type="button"
                             @click="updateAccount(selectedAccount)">
                             Değişiklikleri kaydet
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </template>
+    </Modal>
+    <Modal v-if="isCreateModal" @close="closeCreateModal">
+        <template #header>
+            <div class="flex items-center text-lg">Yeni</div>
+        </template>
+        <template #body>
+            <form>
+                <div class="mb-6">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Başlık</label>
+                    <input
+                        id="title"
+                        v-model="newAccount.accountTitle"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        placeholder=""
+                        required
+                        type="text" />
+                </div>
+                <div class="mb-6">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kullanıcı adı</label>
+                    <input
+                        id="username"
+                        v-model="newAccount.accountName"
+                        autocomplete="username"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        placeholder=""
+                        required
+                        type="text" />
+                </div>
+                <div class="mb-6">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Şifre</label>
+                    <input
+                        id="password"
+                        v-model="newAccount.accountPassword"
+                        autocomplete="new-password"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        placeholder=""
+                        required
+                        type="password" />
+                </div>
+                <div class="mb-6">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Bilgi</label>
+                    <input
+                        id="details"
+                        v-model="newAccount.accountDetails"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        placeholder=""
+                        required
+                        type="text" />
+                </div>
+                <div class="mb-6">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Şirket</label>
+
+                    <select
+                        id="companies"
+                        v-model="newAccount.targetCompanyId"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option v-for="company in companyList" :value="company.id">{{ company.name }}</option>
+                    </select>
+                </div>
+            </form>
+        </template>
+        <template #footer>
+            <div class="flex justify-between">
+                <button
+                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                    type="button"
+                    @click="closeCreateModal">
+                    İptal
+                </button>
+                <div class="flex justify-between">
+                    <div class="px-2">
+                        <button
+                            class="text-white bg-green-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            type="button"
+                            @click="createAccount(newAccount)">
+                            Kaydet
                         </button>
                     </div>
                 </div>
