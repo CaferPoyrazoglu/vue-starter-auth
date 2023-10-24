c0a7d66e9d371979b2462d8be8f833b84cdc1269
Cafer <cafererhan@gmail.com>
2023-10-24 16:18:06 +0300

    sifre degistirme

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,10 +1,16 @@
 <script setup>
 import axiosInstance from '@/api/axiosInstance'
 import { ref } from 'vue'
-import { useMessageStore } from '@/stores'
+import { useAuthStore, useMessageStore } from '@/stores'
+import router from '@/router'
 
 let userInfo = ref(null)
 const messageStore = useMessageStore()
+const authStore = useAuthStore()
+
+const newPasswordRequest = ref({
+    newPassword: '',
+})
 
 await fetchUserInfo()
 
@@ -28,6 +34,38 @@ async function fetchUserInfo() {
     }
 }
 
+async function changePassword() {
+    try {
+        await axiosInstance.post(
+            'auth/change-password',
+            {
+                newPassword: newPasswordRequest.value.newPassword,
+            },
+            {
+                withCredentials: true,
+            }
+        )
+
+        localStorage.removeItem('access_token')
+        localStorage.removeItem('refresh_token')
+
+        authStore.logout()
+
+        window.dispatchEvent(new Event('localStorage'))
+
+        await router.push('/login')
+    } catch (error) {
+        messageStore.status = true
+        if (error.response) {
+            messageStore.message = error.response.data.toString()
+        } else if (error.request) {
+            messageStore.message = 'Sunucuya bağlanılamıyor. Lütfen daha sonra tekrar deneyin.'
+        } else {
+            messageStore.message = 'İsteğiniz gerçekleştirilirken bir hata ile karşılaşıldı.'
+        }
+    }
+}
+
 function getDate(date) {
     return new Date(date).toLocaleString('tr-TR')
 }
@@ -39,7 +77,25 @@ function getDate(date) {
             <h2 class="text-4xl font-extrabold">Profil</h2>
             <hr class="h-px bg-transparent border-0" />
 
-            <div class="bg-white py-7">
+            <div class="columns-2 py-5">
+                <form @submit.prevent="changePassword">
+                    <input
+                        id="helper-text"
+                        v-model="newPasswordRequest.newPassword"
+                        aria-describedby="helper-text-explanation"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Yeni Şifre"
+                        type="password" />
+
+                    <button
+                        class="full text-white bg-green-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                        type="submit">
+                        Şifremi Değiştir
+                    </button>
+                </form>
+            </div>
+
+            <div class="bg-white">
                 <div class="text-gray-700">
                     <div class="grid md:grid-cols-1 text-md">
                         <div class="grid grid-cols-2">
