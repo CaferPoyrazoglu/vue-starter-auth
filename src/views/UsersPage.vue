81ab9cd8c93425222dfa54fcb9b88ebc4d919b60
Cafer <cafererhan@gmail.com>
2023-10-24 15:31:08 +0300

    delete user

--- src/views/UsersPage.vue
+++ src/views/UsersPage.vue
@@ -1,15 +1,69 @@
 <script setup>
 import { ref } from 'vue'
 import { Modal } from 'flowbite-vue'
+import axiosInstance from '@/api/axiosInstance'
+import { useMessageStore } from '@/stores'
+
+const messageStore = useMessageStore()
 
 const isShowModal = ref(false)
+let userList = ref(null)
 
+const selectedUser = ref({
+    id: '',
+    firstName: '',
+    lastName: '',
+})
+
+await fetchUsers()
 function closeModal() {
     isShowModal.value = false
 }
 
-function showModal() {
+function showModal(user) {
     isShowModal.value = true
+    selectedUser.value.id = user.id
+    selectedUser.value.firstName = user.firstName
+    selectedUser.value.lastName = user.lastName
+}
+
+async function fetchUsers() {
+    try {
+        const response = await axiosInstance.get('auth/all-users', {
+            withCredentials: true,
+        })
+
+        userList.value = response.data
+    } catch (error) {
+        messageStore.status = true
+        if (error.response) {
+            messageStore.message = error.response.data.toString()
+        } else if (error.request) {
+            messageStore.message = 'Sunucuya bağlanılamıyor. Lütfen daha sonra tekrar deneyin.'
+        } else {
+            messageStore.message = 'İsteğiniz gerçekleştirilirken bir hata ile karşılaşıldı.'
+        }
+    }
+}
+
+async function deleteUser() {
+  try {
+    const response = await axiosInstance.delete('auth/delete/' + selectedUser.value.id, {
+      withCredentials: true,
+    })
+
+    await fetchUsers()
+    closeModal()
+  } catch (error) {
+    messageStore.status = true
+    if (error.response) {
+      messageStore.message = error.response.data.toString()
+    } else if (error.request) {
+      messageStore.message = 'Sunucuya bağlanılamıyor. Lütfen daha sonra tekrar deneyin.'
+    } else {
+      messageStore.message = 'İsteğiniz gerçekleştirilirken bir hata ile karşılaşıldı.'
+    }
+  }
 }
 </script>
 
@@ -30,17 +84,20 @@ function showModal() {
                     </thead>
                     <tbody>
                         <tr
+                            v-for="user in userList"
+                            :key="user"
                             class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                             <th
                                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                                 scope="row">
-                                Cafer Poyrazoğlu
+                                {{ user.firstName }} {{ user.lastName }}
                             </th>
-                            <td class="px-6 py-4">cafererhan@gmail.com</td>
+                            <td class="px-6 py-4">{{ user.email }}</td>
                             <td class="px-6 py-4">
                                 <a
                                     class="font-medium text-red-600 dark:text-red-500 hover:underline"
-                                    @click="showModal">
+                                    href="#"
+                                    @click="showModal(user)">
                                     Sil
                                 </a>
                             </td>
@@ -56,7 +113,7 @@ function showModal() {
         </template>
         <template #body>
             <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                Kullanıcıyı silmek istediğinize emin misiniz?
+              {{ selectedUser.firstName }} {{ selectedUser.lastName }} adlı kullanıcıyı silmek istediğinize emin misiniz?
             </p>
         </template>
         <template #footer>
@@ -70,7 +127,7 @@ function showModal() {
                 <button
                     class="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     type="button"
-                    @click="closeModal">
+                    @click="deleteUser">
                     Sil
                 </button>
             </div>
